fa6ce47475b907ba9c55bddcf8e6993bd523ecc9
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-11 22:54:00 -0300

    Create first application

---
+++ Aspnet_WebCourse/ICatalogo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Aspnet_WebCourse
+{
+    public interface ICatalogo
+    {
+        List<Livro> GetLivros();
+    }
+}
---
+++ Aspnet_WebCourse/IRelatorio.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Aspnet_WebCourse
+{
+    public interface IRelatorio
+    {
+        Task Imprimir(HttpContext context);
+    }
+}
