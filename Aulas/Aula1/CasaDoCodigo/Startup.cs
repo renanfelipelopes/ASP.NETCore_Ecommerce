538a4bad948d45ec3ae9ec8e5cb7879678fada2a
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-14 22:46:46 -0300

    generating bank

--- Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -31,7 +31,8 @@ namespace CasaDoCodigo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -51,6 +52,11 @@ namespace CasaDoCodigo
                     name: "default",
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
+
+            serviceProvider
+                .GetService<ApplicationContext>()
+                .Database
+                .Migrate();
         }
     }
 }
