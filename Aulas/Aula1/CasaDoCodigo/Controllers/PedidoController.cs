49415345a475fcaad4658f72e2025d4bd7ca9dcd
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-21 11:42:20 -0300

    Saving order item

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -24,8 +24,13 @@ namespace CasaDoCodigo.Controllers
             return View(produtoRepository.GetProdutos());
         }
 
-        public IActionResult Carrinho()
+        public IActionResult Carrinho(string codigo)
         {
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                pedidoRepository.AddItem(codigo);
+            }
+
             Pedido pedido = pedidoRepository.GetPedido(); 
             return View(pedido.Itens);
         }
--- Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -10,6 +10,7 @@ namespace CasaDoCodigo.Repositories
     public interface IPedidoRepository
     {
         Pedido GetPedido();
+        void AddItem(string codigo);
     }
 
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
@@ -19,6 +20,31 @@ namespace CasaDoCodigo.Repositories
         public PedidoRepository(ApplicationContext contexto, IHttpContextAccessor contextAccessor) : base(contexto)
         {
             this.contextAccessor = contextAccessor;
+        }
+
+        public void AddItem(string codigo)
+        {
+            var produto = contexto.Set<Produto>()
+                .Where(p => p.Codigo == codigo)
+                .SingleOrDefault();
+
+            if (produto == null)
+            {
+                throw new ArgumentException("Produto não encontrado.");
+            }
+
+            var pedido = GetPedido();
+            var itemPedido = contexto.Set<ItemPedido>()
+                .Where(i => i.Produto.Codigo == codigo && i.Produto.Id == pedido.Id)
+                .SingleOrDefault();
+
+            if (itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                contexto.Set<ItemPedido>().Add(itemPedido);
+
+                contexto.SaveChanges();
+            }
         }
 
         public Pedido GetPedido()
@@ -31,6 +57,7 @@ namespace CasaDoCodigo.Repositories
                 pedido = new Pedido();
                 dbSet.Add(pedido);
                 contexto.SaveChanges();
+                SetPedidoId(pedido.Id);
             }
 
             return pedido;
